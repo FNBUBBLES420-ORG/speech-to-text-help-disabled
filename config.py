47dc7fc51114e6cf7c3c47d5a46a5ce5f1adae63
Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>
2025-02-26 06:19:07 -0500

    Create config.py
    Signed-off-by: Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>

---
+++ config.py
@@ -0,0 +1,11 @@
+# Configuration for SpeechToTextApp
+
+# GUI Configuration
+APPEARANCE_MODE = "dark"  # Options: "dark", "light"
+THEME_COLOR = "blue"
+FONT = ('Helvetica', 10) # Dont Change this !!!
+
+# Speech Recognition Settings
+AMBIENT_NOISE_ADJUSTMENT_DURATION = 0.5  # Duration to adjust for ambient noise in seconds
+TIMEOUT_DURATION = 3  # Duration to wait before timing out in seconds
+PHRASE_TIME_LIMIT = 3  # Maximum duration for a single phrase in seconds
